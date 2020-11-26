b3cdd8b2795a0089b6c301a9ec45561eb5d8dddb
alestor123 <alestoraldous@gmail.com>
2020-11-26 19:13:56 +0530

    added model

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 var express = require('express'),
 app = express(),
 axios = require('axios'),
-port = process.env.PORT || 3000;
+port = process.env.PORT || 3000,
+Model = require('./model/model'),
 mongoose = require('mongoose');
 app.listen(port, () => console.log(`server running at ${port}`))
